204876ae1e812b377380732dc6f83ed312ce272e
s4nt14go <santiagopergamo@gmail.com>
2021-04-03 12:39:50 -0300

    chore(the-eventbridge-atm): add event logging to lambdas

--- the-eventbridge-atm/lambda-fns/atmConsumer/handler.js
+++ the-eventbridge-atm/lambda-fns/atmConsumer/handler.js
@@ -1,16 +1,19 @@
 // The consuming service (target) Lambda functions
 
 exports.case1Handler = async (event) => {
+  console.log('event', event);
   console.log('--- Approved transactions ---')
   console.log(JSON.stringify(event, null, 2))
 }
 
 exports.case2Handler = async (event) => {
+  console.log('event', event);
   console.log('--- NY location transactions ---')
   console.log(JSON.stringify(event, null, 2))
 }
 
 exports.case3Handler = async (event) => {
+  console.log('event', event);
   console.log('--- Unapproved transactions ---')
   console.log(JSON.stringify(event, null, 2))
   throw Error('Error threw!');
