e66d26bc4d3a13e6f5093b9108a89e90ecbdf067
s4nt14go <santiagopergamo@gmail.com>
2021-04-03 13:07:32 -0300

    revert(the-state-machine): remove pattern
    The dead letter queue won't have effect because lambda is being invoked synchronously while the lambda destinations have effect for asynchronous invocations. Another option would have been invoking lambda putting an SNS in the middle, this way: API Gateway -> SNS -> Lambda; but that's already showed in pattern "the-destined-lambda". Another thing to take into account is that when we `startExecution` we should throw an error, instead of being swallowed as it is.

--- the-state-machine/bin/the-state-machine.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { TheStateMachineStack } from '../lib/the-state-machine-stack';
-
-const app = new cdk.App();
-new TheStateMachineStack(app, 'TheStateMachineStack');
